86ae65059eb332e5a2c254693b667469546b3f14
TopClag <67654323+TopClag@users.noreply.github.com>
2022-04-08 19:04:36 +0100

    last

--- car.py
+++ car.py
@@ -1,1 +1,30 @@
-1
+from cProfile import run
+
+
+class Car:
+    def __init__(self):
+        self.maxspeed = 290
+        self.run = 30000
+        self.engine1 = 'High'
+        self.quality = 'middle'
+        self.width = 1500
+        self.height = 500
+
+    def engine(self):
+        print('Car engining')
+
+    def move(self):
+        print('Car moving')
+    
+    def stop(self):
+        print('Car stoped')
+
+    def race(self):
+        self.run += 1
+
+my_car = Car()
+
+my_car.race()
+my_car.engine()
+my_car.move()
+my_car.stop()
